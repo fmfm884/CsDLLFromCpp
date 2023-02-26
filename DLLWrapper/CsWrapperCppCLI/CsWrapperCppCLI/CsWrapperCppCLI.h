69eddd0dad223c9804660bef74787c1a715ecea9
unknown <genkirad@gmail.com>
2023-02-26 23:21:46 +0900

    add-wraper

---
+++ DLLWrapper/CsWrapperCppCLI/CsWrapperCppCLI/CsWrapperCppCLI.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+// エクスポートとインポートの切り替え
+#ifdef VC_DLL_EXPORTS
+#undef VC_DLL_EXPORTS
+#define VC_DLL_EXPORTS extern "C" __declspec(dllexport)
+#else
+#define VC_DLL_EXPORTS extern "C" __declspec(dllimport)
+#endif
+
+VC_DLL_EXPORTS int __cdecl Add(int a, int b);
+
+using namespace System;
+
+namespace CsWrapperCppCLI {
+	public ref class Class1
+	{
+		// TODO: このクラスのメソッドをここに追加します。
+	};
+}
---
+++ DLLWrapper/DllCs/DllCs/DllcsClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllCs
+{
+    public class DllCsClass
+    {
+
+        public static int Add(int a, int b) => a + b;
+    }
+}
